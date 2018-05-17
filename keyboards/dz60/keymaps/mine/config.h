c0749e0b1c371b5f41ee8f1e2f66d3f9c476348a
Randy D. Wallace Jr <randy@randywallace.com>
2018-05-17 18:24:49 -0400

    More Modifications

--- keyboards/dz60/keymaps/mine/config.h
+++ keyboards/dz60/keymaps/mine/config.h
@@ -1,4 +1,13 @@
 #include "config_common.h"
+
+#define RGB_STA RGB_M_P  //rgb static
+#define RGB_BRE RGB_M_B  //rgb breathe
+#define RGB_RAI RGB_M_R  //rgb rainbow
+#define RGB_SWI RGB_M_SW //rgb swirl
+#define RGB_SNA RGB_M_SN //rgb snake
+#define RGB_KNI RGB_M_K  //rgb knight
+#define RGB_GRA RGB_M_G  //rgb gradient
+
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 #define LEADER_TIMEOUT 700
--- keyboards/dz60/keymaps/mine/keymap.c
+++ keyboards/dz60/keymaps/mine/keymap.c
@@ -3,14 +3,6 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define RGB_STA RGB_M_P  //rgb static
-#define RGB_BRE RGB_M_B  //rgb breathe
-#define RGB_RAI RGB_M_R  //rgb rainbow
-#define RGB_SWI RGB_M_SW //rgb swirl
-#define RGB_SNA RGB_M_SN //rgb snake
-#define RGB_KNI RGB_M_K  //rgb knight
-#define RGB_GRA RGB_M_G  //rgb gradient
-
 #define TMUX_DANCE_TIMEOUT 800
 #define TMUX_PREFIX "a"
 
