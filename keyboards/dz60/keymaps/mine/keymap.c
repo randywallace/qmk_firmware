ece762a49f6d8174fbb0806d2ec22bb1eb0587ce
Randy D. Wallace Jr <randy@randywallace.com>
2018-07-26 00:46:43 -0400

    Update to support LT capslock vim esc

--- keyboards/dz60/keymaps/mine/keymap.c
+++ keyboards/dz60/keymaps/mine/keymap.c
@@ -6,6 +6,8 @@
 #define TMUX_DANCE_TIMEOUT 800
 #define TMUX_PREFIX "a"
 
+#define LOWER_ESC LT(_LOWER, KC_ESC)
+
 enum layers {
   _BASE,
   _LOWER,
@@ -96,11 +98,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `-----------------------------------------------------------'
     */
     [_BASE] = LAYOUT(
-        KC_GESC    , KC_1   , KC_2    , KC_3    , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL     , XXXXXXX, KC_BSPC,
-        KC_TAB     ,          KC_Q    , KC_W    , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC    , KC_RBRC, KC_BSLS,
-        MO(_LOWER) , KC_A   , KC_S    , KC_D    , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,              KC_ENT          ,
-        KC_LSFT    , XXXXXXX, KC_Z    , KC_X    , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT    ,          XXXXXXX,
-        KC_LCTL    ,          KC_LGUI , KC_LALT , KC_SPC ,          KC_SPC ,          KC_SPC , KC_RALT, KC_LEAD, XXXXXXX, MO(_RAISE) ,          KC_RCTL),
+        KC_GESC   , KC_1   , KC_2    , KC_3    , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL     , XXXXXXX, KC_BSPC,
+        KC_TAB    ,          KC_Q    , KC_W    , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC    , KC_RBRC, KC_BSLS,
+        LOWER_ESC , KC_A   , KC_S    , KC_D    , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,              KC_ENT          ,
+        KC_LSFT   , XXXXXXX, KC_Z    , KC_X    , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT    ,          XXXXXXX,
+        KC_LCTL   ,          KC_LGUI , KC_LALT , KC_SPC ,          KC_SPC ,          KC_SPC , KC_RALT, KC_LEAD, XXXXXXX, MO(_RAISE) ,          KC_RCTL),
 
     /* Keymap : Second Layer (WASD and common)
      * ,-----------------------------------------------------------.
